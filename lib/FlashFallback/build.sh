7fd59742a445446bfcfce90b657fadfb3e4cb1af
steffentchr <steffentchr@Steffens-MacBook-Pro.local>
2012-10-01 13:48:45 +0200

    Rename the files for Flash fallback and provide a smalled non-Debug version

--- lib/FlashFallback/build.sh
+++ lib/FlashFallback/build.sh
@@ -2,9 +2,10 @@
 
 rm *swf
 /Applications/Adobe\ Flash\ Builder\ 4/sdks/4.1.0/bin/mxmlc -static-link-runtime-shared-libraries=true -debug=true FlashFallback.as
-mv FlashFallback.swf FlashFallbackDebug.swf
+mv FlashFallback.swf EingebautDebug.swf
 
-###/Applications/Adobe\ Flash\ Builder\ 4/sdks/4.1.0/bin/mxmlc -static-link-runtime-shared-libraries=true -debug=false FlashFallback.as
+/Applications/Adobe\ Flash\ Builder\ 4/sdks/4.1.0/bin/mxmlc -static-link-runtime-shared-libraries=true -debug=false FlashFallback.as
+mv FlashFallback.swf Eingebaut.swf
 
 #echo '' > ~/Library/Preferences/Macromedia/Flash\ Player/Logs/flashlog.txt 
 #tail -f ~/Library/Preferences/Macromedia/Flash\ Player/Logs/flashlog.tx
