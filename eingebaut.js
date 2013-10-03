1c092dc2781e713d8aafde0f6184cc5007a5ffe6
Kalle Kabell <kkabell@gmail.com>
2013-10-03 16:47:26 +0200

    Let iPhone have fullscreen capabilities too, used for 'pinch to enter fullscreen'

--- eingebaut.js
+++ eingebaut.js
@@ -304,7 +304,7 @@ var Eingebaut = function(container, displayDevice, swfLocation, callback){
       if(de.webkitRequestFullScreen&&document.webkitFullscreenEnabled) return true;
     }
     // Second fullscreen mode: Only the video element, relavant mostly for iPad
-    if($this.fullscreenContext=='video' || /iPad/.test(navigator.userAgent)) {
+    if($this.fullscreenContext=='video' || /iPad|iPhone/.test(navigator.userAgent)) {
       var ve = $this.video[0];
       if(ve.requestFullscreen||ve.webkitEnterFullscreen||ve.mozRequestFullScreen) return true;
     }
