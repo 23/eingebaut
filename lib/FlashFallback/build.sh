5f6cd8e07c215bba60f654121024fc91193b1775
Steffen Tiedemann Christensen <steffen@tchr.dk>
2014-02-10 23:04:37 +0100

    Explicitly load RSL from the current server, rather than trying to make non-https requests to fpdownload.adobe.com#

--- lib/FlashFallback/build.sh
+++ lib/FlashFallback/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 rm *swf
-/Applications/Adobe\ Flash\ Builder\ 4/sdks/4.1.0/bin/mxmlc -target-player 10.1 -static-link-runtime-shared-libraries=true -include-libraries+=OSMF.swc -debug=true FlashFallback.as
+/Applications/Adobe\ Flash\ Builder\ 4/sdks/4.1.0/bin/mxmlc -target-player 10.1 -static-link-runtime-shared-libraries=true -include-libraries+=OSMF.swc -debug=true -runtime-shared-library-path=/ FlashFallback.as
 mv FlashFallback.swf EingebautDebug.swf
 
-/Applications/Adobe\ Flash\ Builder\ 4/sdks/4.1.0/bin/mxmlc -target-player 10.1 -static-link-runtime-shared-libraries=true -include-libraries+=OSMF.swc -debug=false FlashFallback.as
+/Applications/Adobe\ Flash\ Builder\ 4/sdks/4.1.0/bin/mxmlc -target-player 10.1 -static-link-runtime-shared-libraries=true -include-libraries+=OSMF.swc -debug=false -runtime-shared-library-path=/ FlashFallback.as
 mv FlashFallback.swf Eingebaut.swf
 
 echo '' > ~/Library/Preferences/Macromedia/Flash\ Player/Logs/flashlog.txt 
